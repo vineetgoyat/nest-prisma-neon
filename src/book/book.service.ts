21f8ff337ac06805ec6a1ce2928641bf2f3c4763
Vineet <vineetgoyat1@gmail.com>
2026-08-08 22:53:31 +0530

    learning prisma and neon combo

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateBookInput } from './dto/create-book.input';
 
 @Injectable()
-export class BookService {}
+export class BookService {
+    constructor(private prisma: PrismaService) {}
+
+    create(data: CreateBookInput) {
+        return this.prisma.book.create({ data });
+    }
+}
