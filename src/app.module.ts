b290465336a9b805a1c08f8814d5f44b44c73878
Vineet <vineetgoyat1@gmail.com>
2026-08-08 22:34:11 +0530

    learning prisma and neon combo

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { BookModule } from './book/book.module';
+import { join } from 'generated/prisma/internal/prismaNamespaceBrowser';
+import { ApolloDriver } from 'node_modules/@nestjs/apollo/dist/drivers/apollo.driver';
+import { ApolloDriverConfig } from 'node_modules/@nestjs/apollo/dist/interfaces/apollo-driver-config.interface';
 
 @Module({
-  imports: [GraphQLModule.forRoot<ApolloDriverConfig>PrismaModule, BookModule],
+  imports: [GraphQLModule.forRoot<ApolloDriverConfig>({
+    driver: ApolloDriver,
+    autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+  }),PrismaModule, BookModule],
   controllers: [AppController],
   providers: [AppService],
 })
