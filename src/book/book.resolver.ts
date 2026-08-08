4a458a21a752ac886d53e6f199b461b74ced7b23
Vineet <vineetgoyat1@gmail.com>
2026-08-08 23:08:10 +0530

    learning prisma and neon combo

--- src/book/book.resolver.ts
+++ src/book/book.resolver.ts
@@ -1,4 +1,13 @@
-import { Resolver } from '@nestjs/graphql';
+import { Query, Resolver } from '@nestjs/graphql';
+import { Book } from './model/book.model';
+import { BookService } from './book.service';
 
-@Resolver()
-export class BookResolver {}
+@Resolver( () => Book )
+export class BookResolver {
+    constructor(private bookService: BookService) {}
+
+    @Query( () => [Book] )
+    getAllBooks(){
+        return this.bookService.findAll();
+    }
+}
