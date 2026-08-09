7b37e8c22ee58ebc4470dc86cc05f6b975d4a9aa
Vineet <vineetgoyat1@gmail.com>
2026-08-09 17:27:20 +0530

    fixed things

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Throttle } from '@nestjs/throttler';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @Throttle({ default: { limit: 3, ttl: 60000 } })
   getHello(): string {
-    return this.appService.getHello();
+    return 'This is a rate-limited route';
   }
 }
